f425987e184e474e3cf4df6e7679910c8abbb2ea
HungFai Koo <jhk888@yahoo.com>
2022-05-23 15:48:15 -0700

    create webapp(webxml) and test folder(Hibernate)

---
+++ src/test/java/util/HibernateUtilTestSuite.java
@@ -0,0 +1,26 @@
+package util;
+
+import org.hibernate.Session;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static bibimbop.util.HibernateUtil.getSession;
+
+public class HibernateUtilTestSuite {
+
+    @Test
+    public void test_getSession_givenProvidedCredentials_returnValidConnection(){
+        // Arrange & Acting
+        try(Session session = getSession()){
+            System.out.println(session);
+
+            // Assert
+            Assertions.assertNotNull(session);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
