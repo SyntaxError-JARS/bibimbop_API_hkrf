ee49870e15a9f1ea17f4e9d2579ea68b6ae8624b
HungFai Koo <jhk888@yahoo.com>
2022-05-25 15:50:12 -0700

    create exceptions authenticationException

---
+++ src/main/java/com/revature/bibimbop/util/exceptions/AuthenticationException.java
@@ -0,0 +1,9 @@
+package com.revature.bibimbop.util.exceptions;
+
+public class AuthenticationException extends RuntimeException {
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
+
