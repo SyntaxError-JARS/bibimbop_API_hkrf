fe0546007bfa84535d77a3c297c83dea81039798
HungFai Koo <jhk888@yahoo.com>
2022-05-28 16:09:52 -0700

    add CreditCardDao

---
+++ src/main/java/com/revature/bibimbop/credit_card/CreditCardDao.java
@@ -0,0 +1,94 @@
+package com.revature.bibimbop.credit_card;
+
+import com.revature.bibimbop.util.HibernateUtil;
+import com.revature.bibimbop.util.interfaces.Crudable;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CreditCardDao implements Crudable<CreditCard> {
+
+    @Override
+    public CreditCard create(CreditCard newCreditCard) {
+        System.out.println(newCreditCard);
+        try {
+            Session session = HibernateUtil.getSession();
+            Transaction transaction = session.beginTransaction();
+            session.save(newCreditCard);
+            transaction.commit();
+            return newCreditCard;
+        } catch (HibernateException | IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    @Override
+    public List<CreditCard> findAll() {
+        try {
+            Session session = HibernateUtil.getSession();
+            Transaction transaction = session.beginTransaction();
+            List<CreditCard> creditCards = session.createQuery("FROM CreditCard").list();
+            transaction.commit();
+            return creditCards;
+        } catch (HibernateException | IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    @Override
+    public CreditCard findById(String username) {
+        try {
+            Session session = HibernateUtil.getSession();
+            Transaction transaction = session.beginTransaction();
+            Creditcard creditcard = session.load(Credit.class, username);
+            transaction.commit();
+            return creditcard;
+        } catch (HibernateException | IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    @Override
+    public boolean update(CreditCard updatedCreditCard) {
+        try {
+            Session session = HibernateUtil.getSession();
+            Transaction transaction = session.beginTransaction();
+            session.merge(updatedCreditCard);
+            transaction.commit();
+            return true;
+        } catch (HibernateException | IOException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    @Override
+    public boolean delete(String creditCard) {
+        try {
+            Session session = HibernateUtil.getSession();
+            Transaction transaction = session.beginTransaction();
+            session.remove(CreditCard);
+            transaction.commit();
+            return true;
+        } catch (HibernateException | IOException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+}
