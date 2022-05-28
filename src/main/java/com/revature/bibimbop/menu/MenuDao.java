f34a9384cf245be40567989bd34747586bc037ae
HungFai Koo <jhk888@yahoo.com>
2022-05-28 14:04:56 -0700

    added MenuDao

---
+++ src/main/java/com/revature/bibimbop/menu/MenuDao.java
@@ -0,0 +1,95 @@
+package com.revature.bibimbop.menu;
+
+import com.revature.bibimbop.menu.MenuType;
+import com.revature.bibimbop.util.HibernateUtil;
+import com.revature.bibimbop.util.interfaces.Crudable;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MenuDao implements Crudable<Menu> {
+
+    @Override
+    public Menu create(Menu newMenu) {
+        try {
+            Session session = HibernateUtil.getSession();
+            Transaction transaction = session.beginTransaction();
+            session.save(newMenu);
+            transaction.commit();
+            return newMenu;
+        } catch (HibernateException | IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    @Override
+    public List<Menu> findAll() {
+        try {
+            Session session = HibernateUtil.getSession();
+            Transaction transaction = session.beginTransaction();
+            List<Menu> abilities = session.createQuery("FROM Menu").list();
+            transaction.commit();
+            return abilities;
+        } catch (HibernateException | IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    @Override
+    public Menu findById(String id) {
+        try {
+            Session session = HibernateUtil.getSession();
+            Transaction transaction = session.beginTransaction();
+            Menu menu = session.get(Menu.class, id);
+            transaction.commit();
+            return menu;
+        } catch (HibernateException | IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    @Override
+    public boolean update(Menu updatedMenu) {
+        try {
+            Session session = HibernateUtil.getSession();
+            Transaction transaction = session.beginTransaction();
+            session.merge(updatedMenu);
+            transaction.commit();
+            return true;
+        } catch (HibernateException | IOException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    @Override
+    public boolean delete(String id) {
+        try {
+            Session session = HibernateUtil.getSession();
+            Transaction transaction = session.beginTransaction();
+            Menu menu = session.load(Menu.class, id);
+            session.remove(menu);
+            transaction.commit();
+            return true;
+        } catch (HibernateException | IOException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+}
