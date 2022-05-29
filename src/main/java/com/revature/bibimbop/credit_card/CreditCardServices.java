fae1d320b34f0190d307a2a0bf50cf14cb77bd86
HungFai Koo <jhk888@yahoo.com>
2022-05-28 17:33:39 -0700

    updated customer-customer

--- src/main/java/com/revature/bibimbop/credit_card/CreditCardServices.java
+++ src/main/java/com/revature/bibimbop/credit_card/CreditCardServices.java
@@ -2,9 +2,9 @@ package com.revature.bibimbop.credit_card;
 
 import com.revature.bibimbop.credit_card.CreditCardDao;
 import com.revature.bibimbop.menu.MenuDao;
-import com.revature.bibimbop.exceptions.InvalidRequestException;
-import com.revature.bibimbop.exceptions.ResourcePersistenceException;
 import com.revature.bibimbop.credit_card.CreditCard;
+import com.revature.bibimbop.util.exceptions.InvalidRequestException;
+import com.revature.bibimbop.util.exceptions.ResourcePersistenceException;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -27,12 +27,12 @@ public class CreditCardServices {
         if(persistedCreditCard == null) {
             throw new ResourcePersistenceException("Credit card was not persisted to the database");
         }
-        return newCreditCardData;
+        return newCreditCard;
     }
 
 
     public ArrayList<CreditCard> readAll() throws IOException {
-        ArrayList<CreditCard> creditCard = creditCardDao.findAll();
+        ArrayList<CreditCard> creditCard = (ArrayList<CreditCard>) creditCardDao.findAll();
         return creditCard;
     }
     public CreditCard readByID(String username){
@@ -55,9 +55,6 @@ public class CreditCardServices {
         if(newCreditCard == null) return false;
         if(newCreditCard.getCardNumber() == null || newCreditCard.getCardNumber().trim().equals((""))) return false;
         if(newCreditCard.getCardName() == null || newCreditCard.getCardName().trim().equals((""))) return false;
-        if(newCreditCard.getExpDate() == null || newCreditCard.getExpDate().trim().equals((""))) return false;
-        if(newCreditCard.getZipCode() == 0 ) return false;
-        if(newCreditCard.getCreditLimit() == 0 ) return false;
         if(newCreditCard.getUsername() == null || newCreditCard.getUsername().trim().equals((""))) return false;
         return true;
     }
--- src/main/java/com/revature/bibimbop/customer/Customer.java
+++ src/main/java/com/revature/bibimbop/customer/Customer.java
@@ -8,32 +8,43 @@ import javax.persistence.*;
 @Table(name = "customer")
 public class Customer {
 
-    private String fname;
-    private String lname;
     @Id
     private String username;
+    private String fname;
+    private String lname;
+
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
-    private Int balance;
+
+    private int balance;
+
+    @Column(name = "is_admin")
+    private boolean isAdmin;
 
 
 
-    public Customer(String fname, String lname, String username, String password, Int balance) {
+    public Customer(String username, String fname, String lname,  String password, int balance, boolean isAdmin) {
         super();
         this.fname = fname;
         this.lname = lname;
         this.username = username;
         this.password = password;
         this.balance = balance;
+        this.isAdmin = isAdmin;
     }
 
     public Customer(String password){
         this.password = password;
     }
 
-    public Customer() {
-
+    public String getUsername() {
+        return username;
     }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 
     // Getters & Setters
     public String getFname() {
@@ -54,13 +65,7 @@ public class Customer {
         this.lname = lname;
     }
 
-    public String getUsername() {
-        return username;
-    }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public String getPassword() {
         return password;
@@ -70,21 +75,29 @@ public class Customer {
         this.password = password;
     }
 
-    public Int getBalance() {
+    public int getBalance() {
         return balance;
     }
 
-    public void setBalance(Int balance) {
+    public void setBalance(int balance) {
         this.balance = balance;
+    }
+
+    public boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
     }
 
     @Override
     public String toString() {
-        return "Trainer{" +
-                "fname='" + fname + '\'' +
+        return "Customer{" +
+                "username='" + username + '\'' +
+                ", fname='" + fname + '\'' +
                 ", lname='" + lname + '\'' +
-                ", email='" + email + '\'' +
-                ", dob='" + dob + '\'' +
+                ", balance='" + balance + '\'' +
                 '}';
     }
 
