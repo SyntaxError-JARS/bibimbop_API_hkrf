97941b439ba4e89f137d2f49de529a2e8f98eb1a
HungFai Koo <jhk888@yahoo.com>
2022-05-30 13:58:20 -0700

    updated ContextLoaderListener and Customer Servlet

--- src/main/java/com/revature/bibimbop/customer/CustomerServlet.java
+++ src/main/java/com/revature/bibimbop/customer/CustomerServlet.java
@@ -3,13 +3,13 @@ package com.revature.bibimbop.customer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
-
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class CustomerServlet {
+public class CustomerServlet extends HttpServlet {
 
     private final CustomerDao cDao;
     private final ObjectMapper mapper;
